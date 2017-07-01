3bd1a034d3d859df326799c0aa53b9fcddb5b688
ihadanny <ido.hadanny@gmail.com>
2017-07-01 23:17:49 +0300

    working with softmax instead of log-loss of 1 output

--- cartpole.py
+++ cartpole.py
@@ -14,7 +14,7 @@ env.reset()
 H_SIZE = 10 #Number of hidden layer neurons
 ETA = 1e-2 #Learning Rate
 GAMMA = 0.99 #Discount factor
-
+REG = 0.0
 INPUT_DIM = 4 #Input dimensions
 
 
@@ -28,28 +28,31 @@ input_x = tf.placeholder(tf.float32, [None,INPUT_DIM] , name="input_x")
 W1 = tf.get_variable("W1", shape=[INPUT_DIM, H_SIZE],
            initializer=tf.contrib.layers.xavier_initializer())
 layer1 = tf.nn.relu(tf.matmul(input_x,W1))
-W2 = tf.get_variable("W2", shape=[H_SIZE, 1],
+W2 = tf.get_variable("W2", shape=[H_SIZE, 2],
            initializer=tf.contrib.layers.xavier_initializer())
-score = tf.matmul(layer1,W2)
+logits = tf.matmul(layer1,W2)
 
-#TODO: we want to generalize to many outputs, not just a single neuron
-#action_probs = sess.run(y,feed_dict={observation: obsrv})
-#action = np.argmax(np.multinomial(1,action_probs))
-probability = tf.nn.sigmoid(score)
+#probability = tf.nn.sigmoid(score)
 
 #From here we define the parts of the network needed for learning a good policy.
 tvars = tf.trainable_variables()
-input_y = tf.placeholder(tf.float32,[None,1], name="input_y")
-advantages = tf.placeholder(tf.float32,name="reward_signal")
+input_y = tf.placeholder(tf.float32,[None,2], name="input_y")
+advantages = tf.placeholder(tf.float32,[None,1], name="reward_signal")
 
 # The loss function. This sends the weights in the direction of making actions 
 # that gave good advantage (reward over time) more likely, and actions that didn't less likely.
-loglik = tf.log(input_y*(input_y - probability) + (1 - input_y)*(probability))
-loss = -tf.reduce_mean(loglik * advantages) 
+
+probs = tf.nn.softmax(logits)
+probs_log = tf.log(probs)
+ce1 = tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=input_y)
+#ce2 = -tf.reduce_sum(input_y * probs_log, reduction_indices=[1])
+# following lines do inner product
+mul_by_adv = tf.multiply(advantages, tf.reshape(ce1, [-1, 1]))
+loss_new = tf.reduce_sum(mul_by_adv) 
 
 adam = tf.train.AdamOptimizer(learning_rate=ETA) # Adam optimizer
 # next line returns for each layer the a (grads, vars) pair, but we dont want to use it as-is, we want to accumulate grad
-newGrads = adam.compute_gradients(loss, var_list=tvars)
+newGrads = adam.compute_gradients(loss_new, var_list=tvars)
 updateGrads = adam.apply_gradients(newGrads)
 
 def discount_rewards(r):
@@ -80,11 +83,11 @@ with tf.Session() as sess:
         x = np.reshape(input_initial,[1,INPUT_DIM])
         
         # Run policy network 
-        tfprob = sess.run(probability,feed_dict={input_x: x})
-        action = 1 if np.random.uniform() < tfprob else 0
+        tfprob = sess.run(probs,feed_dict={input_x: x})
         
+        y = np.random.multinomial(1,tfprob[0])
+        action = np.argmax(y)
         xs.append(x) #Store x
-        y = 1 if action == 0 else 0
         ys.append(y)
 
         # take action for the state
@@ -110,9 +113,9 @@ with tf.Session() as sess:
             sess.run(updateGrads,feed_dict={input_x: epx, input_y: epy, advantages: discounted_epr})
             # Print details of the present model
             running_reward = reward_sum if running_reward is None else running_reward * 0.99 + reward_sum * 0.01
-            print 'Episode %d: Average reward for episode in this batch %f.  Running average reward %f.' % (episode_number, reward_sum/batch_size, running_reward/batch_size)
+            print 'Episode %d: Average reward for episode %f.  Running average reward %f.' % (episode_number, reward_sum, running_reward)
                 
-            if running_reward/batch_size > 195: 
+            if running_reward > 195: 
                 print "Task solved in",episode_number,'episodes'
                 break
                     
